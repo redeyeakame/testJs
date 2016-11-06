12ba04e5c9b7e905b0811740e8c31ea33aa05820
太郎 <redeyeakame719@gmail.com>
2016-11-06 13:50:30 +0900

    色

--- testJs/js/main.js
+++ testJs/js/main.js
@@ -2,8 +2,7 @@
 window.onload = function () {
     var cvs = document.getElementById('canvas');
     var ctx = cvs.getContext('2d');
+    ctx.fillStyle = "rgb(0,255,0)";
     ctx.fillRect(0, 0, 100, 100);
-
-
 
 }
